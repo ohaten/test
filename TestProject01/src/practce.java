27268dd37f7e90c9d4f3fddfd678976e5d65bc2c
ohaten <suisoku_houraika@docomo.ne.jp>
2019-02-06 20:48:33 +0900

    sagyo0206

---
+++ TestProject01/src/practce.java
@@ -0,0 +1,12 @@
+
+public class practce {
+
+	public static void main(String[] args) {
+		// if文
+    for(int i=1;i<=55;i++){
+    	System.out.println("★");
+    }
+
+	}
+
+}
---
+++ sample/src/jp/co/internous/action/Main.java
@@ -0,0 +1,11 @@
+package jp.co.internous.action;
+
+public class Main {
+	public static void main(String[] arge){
+        System.out.println("Hellow World");
+	    System.out.println(gokei());
+	  }
+    public static int gokei(){
+	           return 1+1;
+ }
+}
